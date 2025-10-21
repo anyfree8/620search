53b68df2acfd395f147638c1bc36ba21a402784e
a.komnatskii <a.komnatskii@vk.team>
2025-10-21 15:28:59 +0300

    add data index

--- dev/index.py
+++ dev/index.py
@@ -10,7 +10,7 @@ def delta_decode(first, deltas):
     return result
 
 class ReverseIndex:
-    def __init__(self, file_path = ''):
+    def __init__(self, file_path = 'data/wikipedia_delta_index.pb'):
         self.index = data.wikipedia_dataset_pb2.InvertedIndex()
         self.file_path = file_path
         self.load_index()
@@ -29,7 +29,7 @@ class ReverseIndex:
             self.index.ParseFromString(f.read())
 
 class CoordinateIndex:
-    def __init__(self, file_path = ''):
+    def __init__(self, file_path = 'data/wikipedia_pos_index.pb'):
         self.index = data.wikipedia_dataset_pb2.PositionalIndex()
         self.file_path = file_path
         self.load_index()
@@ -46,6 +46,28 @@ class CoordinateIndex:
                         )
         return pos_ids
     
+    def load_index(self):
+        with open(self.file_path, 'rb') as f:
+            self.index.ParseFromString(f.read())
+
+class DataIndex:
+    def __init__(self, file_path = 'data/documents'):
+        self.index = data.wikipedia_dataset_pb2.DocumentCollection()
+        self.file_path = file_path
+        self.load_index()
+
+    def get(self, doc_id):
+        for document in self.index.documents:
+            doc_ids.append(document.id)
+            if int(document.id) == doc_id:
+                return {
+                    "doc_id": document.id,
+                    "text": document.text,
+                    "title": document.title,
+                    "url": document.url
+                }
+        return None
+            
     def load_index(self):
         with open(self.file_path, 'rb') as f:
             self.index.ParseFromString(f.read())
